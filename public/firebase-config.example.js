f1101f75326c52e1b765eb6aff2be84d89f7af3f
594katchang-source <594katchang@gmail.com>
2026-06-23 19:38:43 +0800

    Add Firebase config example without secrets

---
+++ public/firebase-config.example.js
@@ -0,0 +1,8 @@
+window.TEACHING_WORDCLOUD_FIREBASE_CONFIG = {
+  apiKey: "REPLACE_WITH_RESTRICTED_BROWSER_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_WEB_APP_ID"
+};
